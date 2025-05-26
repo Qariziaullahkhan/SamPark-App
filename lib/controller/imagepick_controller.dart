c0866413399e654af7ebaa659c60df9caf9fe625
Zia Ullah <malikziakhan0315@gmail.com>
2025-05-26 20:22:23 +0500

    upgrade the flutter project

---
+++ lib/controller/imagepick_controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ImagepickController extends GetxController {
+  final ImagePicker picker = ImagePicker();
+  Future<String> pickImage() async {
+    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      // print(image.path);
+    } else {
+      // print("Image not picked");
+    }
+    return "";
+  }
+}
